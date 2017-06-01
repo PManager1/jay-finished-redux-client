db158cbd82af7281f49539145df4c57e3d835d33
jpca999 <jpca999@gmail.com>
2017-06-01 19:55:05 -0400

    107 started

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reduxThunk from 'redux-thunk';
 import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
-
+import Signup from './components/auth/signup';
 
 import RequireAuth from './components/auth/require_auth';
 
@@ -33,7 +33,7 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
-
+        <Route path="signup" component={Signup} />
       </Route>
     </Router>
   </Provider>
