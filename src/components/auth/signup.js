f34b1a6f1fb3cd90db7a6c6b4ce092881db3fb46
jpca999 <jpca999@gmail.com>
2017-06-01 20:05:48 -0400

    107 done

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,49 +4,45 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
 
-  handleFormSubmit({ email, password }) {
-    // Need to do something to log user in
-    this.props.signinUser({ email, password });
-  }
-
-  renderAlert(){
-    if (this.props.errorMessage) {
-      return(
-          <div className="alert alert-danger">
-            <strong> Oops! </strong>  {this.props.errorMessage}
-          </div>
-      );
-    }
-
-  }
 
 render() {
-  const { handleSubmit, fields: { email, password }} = this.props;
+
+const { handleSubmit, fields: {email, password, passwordConfirm }} = this.props;
 
   return (
-      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <fieldset className="form-group">
-          <label>Email:</label>
-          <input {...email} className="form-control" />
-        </fieldset>
-        <fieldset className="form-group">
-          <label>Password:</label>
-          <input {...password} type="password" className="form-control" />
-        </fieldset>
-          {this.renderAlert()}
-        <button action="submit" className="btn btn-primary">Sign in</button>
-      </form>
+    <form>
+      <fieldset className="form-group">
+        <label> Email </label>
+        <input className="form-control" {...email} />
+      </fieldset>
 
+      <fieldset className="form-group">
+        <label> Password </label>
+        <input className="form-control" {...password}  type="password" />
+      </fieldset>
+
+      <fieldset className="form-group">
+        <label> Confirm Password </label>
+        <input className="form-control" {...passwordConfirm} type="password" />
+      </fieldset>
+      <button action="submit" className="btn btn-primary"> SignUp  </button>
+    </form>
   );
 }
 }
 
-function mapStateToProps(state) {
-  return { errorMessage: state.auth.error };
-}
 
 
 export default reduxForm({
-  form: 'signin',
-  fields: ['email', 'password']
-}, mapStateToProps, actions)(Signup);
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm']
+})(Signup);
+
+
+
+
+
+
+
+
+//
